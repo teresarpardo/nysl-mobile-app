b6ef13463d16ae838a1c53d3d84852f93ea6ef8d
teresarpardo <teresarpardo@icloud.com>
2019-05-05 09:48:44 +0200

    delete firebase

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "./firebase";
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
 
